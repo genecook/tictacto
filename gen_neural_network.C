b54a9fd96c60790f88018c2b92059fe887c0cea1
Gene Cook <genec_1@earthlink.net>
2021-09-28 09:39:21 -0600

    cleanup games reader method

--- gen_neural_network.C
+++ gen_neural_network.C
@@ -30,26 +30,46 @@ public:
     write_nets_builder_file(neural_nets_builder_file);
   };
 
+  enum SQUARE { O=1, X=2, FREE=0 };
+
+  unsigned long long encode_move(unsigned long long moves, int index, unsigned int side) {
+    unsigned int next_move = (index << 2) | side;  // encode move: board square index + side
+    return (moves << 6) | next_move;               // moves then will include all moves made during some game
+  };
+
+  struct move {
+    move() {};
+    move(int _board_index, int _side) : board_index(_board_index), side(_side) {};
+    int board_index;
+    int side;
+  };
+  
+  void extract_states(int game_number, unsigned long long encoded_moves, bool is_win, std::vector<struct move> &moves) {
+    std::cout << "game " << game_number << std::endl;
+    std::cout << "   encoded moves: 0x" << std::hex << encoded_moves << std::dec << std::endl;
+    std::cout << "   outcome: " << (is_win ? "WIN" : "DRAW") << std::endl;
+      
+    for (auto mi = moves.begin(); mi != moves.end(); mi++) {
+       std::cout << "   move:  board index=" << mi->board_index << ", side=" << ((mi->side==X) ? "X" : "O") << std::endl;
+    }
+  };
+  
   void read_games_files(std::string &games_file) {
     pt::ptree tree;
     pt::read_xml(games_file, tree);
 
     BOOST_FOREACH(pt::ptree::value_type &v, tree.get_child("games")) {
-      std::cout << v.first.data() << std::endl;
-       
       pt::ptree game_subtree = v.second;
 
       std::string at = v.first + ".<xmlattr>";
       
       const pt::ptree & attributes = v.second.get_child("<xmlattr>", empty_ptree());
-      std::cout << "Extracting attributes from " << at << ":" << std::endl;
 
       int game_number = -1;
       unsigned long long encoded_moves = 0;
       bool is_win = false;
       
       BOOST_FOREACH(const pt::ptree::value_type &va, attributes) {
-	std::cout << "First: " << va.first.data() << " Second: " << va.second.data() << std::endl;
 	if (!strcmp(va.first.data(),"number"))
 	  sscanf(va.second.data().c_str(),"%d",&game_number);
 	else if (!strcmp(va.first.data(),"moves_encoded"))
@@ -58,26 +78,18 @@ public:
 	  is_win = !strcmp(va.first.data(),"outcome") && (va.second.data() == "WIN");
       }
       
-      std::cout << "   game number: " << game_number << std::endl;
-      std::cout << "   encoded moves: 0x" << std::hex << encoded_moves << std::dec << std::endl;
-      std::cout << "   is win? " << is_win << std::endl;
-
+      std::vector<struct move> moves;
+      
       BOOST_FOREACH(pt::ptree::value_type &mv, game_subtree.get_child("moves")) {
-	std::cout << mv.first.data() << std::endl;
-
         std::string at = mv.first + ".<xmlattr>";
       
         const pt::ptree & attributes = mv.second.get_child("<xmlattr>", empty_ptree());
-        std::cout << "Extracting attributes from " << at << ":" << std::endl;
 
 	int move_number = -1;
 	int board_index = -1;
 	int side = O;
 	  
-	BOOST_FOREACH(const pt::ptree::value_type &va, attributes) {
-	
-	  std::cout << "First: " << va.first.data() << " Second: " << va.second.data() << std::endl;
-	  
+	BOOST_FOREACH(const pt::ptree::value_type &va, attributes) {	
 	  if (!strcmp(va.first.data(),"number"))
 	    sscanf(va.second.data().c_str(),"%d",&move_number);
 	  else if (!strcmp(va.first.data(),"index"))
@@ -86,10 +98,10 @@ public:
 	    side = !strcmp(va.first.data(),"side") && (va.second.data() == "X");
        	}
 	
-        std::cout << "   move number: " << move_number << std::endl;
-	std::cout << "   board index:" << board_index << std::endl;
-	std::cout << "   side: " << ((side==X) ? "X" : "O") << std::endl;
+        moves.push_back(move(board_index,side));
       }
+
+      extract_states(game_number,encoded_moves,is_win,moves);
     }
 
   };
