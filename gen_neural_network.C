721c2d07f5abd7b943c006d5a29205074fdc7254
Gene Cook <genec_1@earthlink.net>
2021-09-29 10:15:50 -0600

    add in 'starter' neural net classes

--- gen_neural_network.C
+++ gen_neural_network.C
@@ -6,6 +6,7 @@
 #include <exception>
 #include <iostream>
 
+#include <stdlib.h>
 #include <time.h>
 
 #include <boost/property_tree/ptree.hpp>
@@ -19,6 +20,85 @@ const pt::ptree& empty_ptree() {
   return t;
 }
 
+class Qneuron {
+public:
+  Qneuron() {};
+  Qneuron(unsigned long long _state, unsigned long long _input, bool _input_layer, bool _output_layer)
+    : state(_state), input_layer(_input_layer), output_layer(_output_layer) {
+    AddConnection(input_biases[_input]);
+    srand48(time(NULL));
+  };
+
+  bool InputLayer() { return input_layer; };
+  bool OutputLayer() { return output_layer; };
+  unsigned long long NextState() { return state; }; // this state's value will be used as a 'connection' to any next state(s)
+  void AddConnection(unsigned long long _input) {
+    input_biases[_input] = drand48(); // bias between 0 and 1.0
+  };
+  float Bias(unsigned long long input) { return input_biases[input]; };
+  void SetBias(unsigned long long input, float nval) { input_biases[input] = nval; };
+  
+private:
+  unsigned long long state;
+  bool input_layer;
+  bool output_layer;
+  std::map<unsigned long long, float> input_biases; // biases for each input
+};
+
+class QneuronNetwork {
+public:
+  QneuronNetwork() {};
+  void AddNeuron(unsigned long long state, unsigned long long previous_state, bool input_layer=false, bool output_layer=false) {
+    if (neurons.find(state) != neurons.end()) {
+      // add connection to existing neuron...
+      neurons[state].AddConnection(previous_state);
+    } else {
+      neurons[state] = Qneuron(state,previous_state,input_layer,output_layer);
+    }
+
+    std::map<unsigned long long, std::vector<unsigned long long>>::iterator ci = connections.find(previous_state);
+
+    if (ci == connections.end()) {
+      connections[previous_state].push_back(state);
+      return;
+    }
+    
+    bool connection_already_there = false;
+    for (auto si = (ci->second).begin(); si != (ci->second).end(); si++) {
+       if ( (*si) == state) {
+	 connection_already_there = true;
+	 break;
+       }
+    }
+    if (!connection_already_there)
+      connections[previous_state].push_back(state);
+  };
+
+  void ShowConnections() {
+    std::cout << " # of neurons: " << neurons.size() << ", # of connections: " << connections.size() << std::endl;
+    
+    std::cout << "Neurons:";
+    for (auto ni = neurons.begin(); ni != neurons.end(); ni++) {
+      std::cout << " 0x" << std::hex << ni->second.NextState() << std::dec;
+      std::cout << (ni->second.InputLayer() ? "(input)" : "") << (ni->second.OutputLayer() ? "(output)" : "");
+    }
+    std::cout << "\n";
+    std::cout <<   "Connections:\n";
+    for (auto ci = connections.begin(); ci != connections.end(); ci++) {
+      std::cout << "             0x" << std::hex << ci->first << " --->" << std::dec;
+       for (auto si = (ci->second).begin(); si != (ci->second).end(); si++) {
+	 std::cout << " 0x" << std::hex << (*si) << std::dec;
+       }
+       std::cout << "\n";
+    }
+    std::cout << std::endl;
+  };
+  
+private:
+  std::map<unsigned long long, Qneuron> neurons; // set of all neurons, indexed by state
+  std::map<unsigned long long, std::vector<unsigned long long>> connections; // connections between neurons...
+};
+
 enum SQUARE { O=1, X=2, FREE=0 };
 
 class tictacto_neural_network_generator {
@@ -29,8 +109,6 @@ public:
     build_network();
     write_nets_builder_file(neural_nets_builder_file);
   };
-
-  enum SQUARE { O=1, X=2, FREE=0 };
 
   unsigned long long encode_move(unsigned long long moves, int index, unsigned int side) {
     unsigned int next_move = (index << 2) | side;  // encode move: board square index + side
@@ -43,15 +121,29 @@ public:
     int board_index;
     int side;
   };
-  
+
   void extract_states(int game_number, unsigned long long encoded_moves, bool is_win, std::vector<struct move> &moves) {
     std::cout << "game " << game_number << std::endl;
     std::cout << "   encoded moves: 0x" << std::hex << encoded_moves << std::dec << std::endl;
     std::cout << "   outcome: " << (is_win ? "WIN" : "DRAW") << std::endl;
-      
-    for (auto mi = moves.begin(); mi != moves.end(); mi++) {
-       std::cout << "   move:  board index=" << mi->board_index << ", side=" << ((mi->side==X) ? "X" : "O") << std::endl;
+
+    unsigned long long previous_board_state = 0, next_board_state = 0;
+    
+    for (unsigned int i = 0; i < moves.size(); i++) {
+       std::cout << "   move:  board index=" << moves[i].board_index << ", side=" << ((moves[i].side==X) ? "X" : "O");
+
+       previous_board_state = next_board_state;
+
+       int binx = moves[i].board_index * 2;
+       next_board_state = (next_board_state | (3 << binx)) ^ (3 << binx) | (moves[i].side << binx);
+
+       std::cout << " prev-state: 0x" << std::hex << previous_board_state << " next-state: 0x"
+		 << next_board_state << std::dec << std::endl;
+       
+       my_network.AddNeuron( next_board_state, previous_board_state, (i==0), (i==(moves.size()-1) ) );
     }
+
+    my_network.ShowConnections();
   };
   
   void read_games_files(std::string &games_file) {
@@ -94,8 +186,8 @@ public:
 	    sscanf(va.second.data().c_str(),"%d",&move_number);
 	  else if (!strcmp(va.first.data(),"index"))
 	    sscanf(va.second.data().c_str(),"%d",&board_index);
-	  else
-	    side = !strcmp(va.first.data(),"side") && (va.second.data() == "X");
+	  else if (!strcmp(va.first.data(),"side"))
+	    side = (va.second.data() == "X") ? X : O;
        	}
 	
         moves.push_back(move(board_index,side));
@@ -113,7 +205,7 @@ public:
   };
   
 private:
-  
+  QneuronNetwork my_network;
 };
 
 int main(int argc, char **argv) {
